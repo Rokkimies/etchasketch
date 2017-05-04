a7dd5b6c9b83ee32ffa77d32da0295d58a05cff5
Henrik Pesonen <henrik.pesonen@gmail.com>
2017-05-04 03:53:05 +0300

    Mostly working version, still trying to figure out how to fill the whole area for drawing

--- etch.js
+++ etch.js
@@ -1,14 +1,36 @@
+var size = 16;
+
 $(document).ready(function(){
-
-  for (var i = 0; i< 16; i++){
-    var row = $("<div id='table-row'></div>").appendTo("#wrapper");
-
-    for(var x = 0; x< 16; x++){
-      $("<div id='pixel'></div>").appendTo(row);
-    }
-  }
-
-  $("div#pixel").mouseenter(function(){
-    $(this).css("background-color","white");
-  });
+draw(size);
 });
+
+
+function refresh(){
+  var ask = true;
+  while(ask){
+    size = askSquares();
+    if(!isNaN(size)){
+      ask = false;}}
+
+    $(".pixel").each(function(){
+      $(this).remove();})
+
+    $(".table-row").each(function(){
+      $(this).remove();})
+    draw(size);
+};
+
+function draw(size){
+
+  for (var i = 0; i< size; i++){
+    var row = $("<div class='table-row'></div>").appendTo("#etchWrapper");
+    for(var x = 0; x< size; x++){
+      $("<div class='pixel'></div>").appendTo(row);}}
+
+  $("div.pixel").mouseenter(function(){
+    $(this).css("background-color","green");})
+  };
+
+function askSquares(){
+  return prompt("How many squares per side");
+};
